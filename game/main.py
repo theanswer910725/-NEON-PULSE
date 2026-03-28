fe3b520b0abd0d0c611de17a8886200182f2f11e
lilinwei <work_lilinwei@foxmail.com>
2026-03-28 15:18:23 +0800

    feat: add basic game framework

---
+++ game/main.py
@@ -0,0 +1,39 @@
+import pygame
+import sys
+from constants import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Neon Runner")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            self.clock.tick(FPS)
+            self.handle_events()
+            self.update()
+            self.draw()
+        pygame.quit()
+        sys.exit()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
